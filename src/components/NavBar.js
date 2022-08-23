b05bc39af004bc3c30fde4eed69ca2feb89b4a3f
Lautaro Minue <minue.lautaro7@gmail.com>
2022-08-22 22:55:46 -0300

    desafio navbar terminado

---
+++ src/components/NavBar.js
@@ -0,0 +1,25 @@
+import './NavBar.css';
+
+const NavBar = () => {
+    return(
+        <nav>
+            <div>
+                <img src='https://community.cloudflare.steamstatic.com/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXQ9QVcJY8gulROWEXTTOG_xJ2cUE97MgposLWsJzhs0uHPdHNHtITvwtbaxaHwZrjXkD1T7sEmi-2W9In2ilKy-UI-ZmGnd9THewdsfxiOrSe6AUNs/360fx360f' className='nav__icon' />
+            </div>
+            <div className='nav__menu'>
+                <div>
+                    <ul>Home</ul>
+                </div>
+                <div>
+                    <ul>Productos</ul>
+                </div>
+                <div>
+                    <ul>Contacto</ul>
+                </div>
+            </div>
+            
+        </nav>
+    )
+}
+
+export default NavBar;
