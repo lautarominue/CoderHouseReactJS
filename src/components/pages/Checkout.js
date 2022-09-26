a284d1294e3edc0c8bcbb07d62a9b7272b703335
Lautaro Minue <minue.lautaro7@gmail.com>
2022-09-26 19:48:17 -0300

    desafio cart view 3.0

--- src/components/pages/Checkout.js
+++ src/components/pages/Checkout.js
@@ -2,12 +2,15 @@ import { useContext } from "react"
 import { CartContext } from "../context/CartContext"
 import ListCartWidget from "../ListCartWidget/ListCartWidget"
 import PriceTotal from "../PriceTotal/PriceTotal"
+import { Link } from 'react-router-dom';
 
 
 const Checkout = () => {
     const {cartProducts} = useContext(CartContext)
     return(
         <>
+        {cartProducts.length == 0 ? <Link to="/"> <ul>Carrito vacio - ir al HOME para comprar productos</ul></Link> : ""}
+        
         <div>
             {cartProducts.map( (data) => {
                 return <ListCartWidget data={data} />
